c382ee5c5a6551cd71056376743aa3f5089d4534
D.Yang <koyangslash@gmail.com>
2017-03-13 18:06:58 +0800

    add Travis CI

---
+++ .travis/sync.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Decrypt the private key
+openssl aes-256-cbc -K $encrypted_4ab3a980d35d_key -iv $encrypted_4ab3a980d35d_iv -in .travis/ssh_key.enc -out ~/.ssh/id_rsa -d
+# Set the permission of the key
+chmod 600 ~/.ssh/id_rsa
+# Start SSH agent
+eval $(ssh-agent)
+# Add the private key to the system
+ssh-add ~/.ssh/id_rsa
+# Copy SSH config
+cp .travis/ssh_config ~/.ssh/config
+# Set Git config
+git config --global user.name "D.Yang"
+git config --global user.email koyangslash@gmail.com
+# Add coding remote
+git remote add coding git@git.coding.net:yangyang0507/mybatis-plus-doc.git
+# Push
+git push coding
