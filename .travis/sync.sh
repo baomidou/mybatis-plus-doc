6ee60bebcc3ad20255316c333f32573aaa34bd85
D.Yang <koyangslash@gmail.com>
2018-09-05 15:47:24 +0800

    update travis

--- .travis/sync.sh
+++ .travis/sync.sh
@@ -20,10 +20,20 @@ git config --global user.name "D.Yang"
 git config --global user.email koyangslash@gmail.com
 git config --global push.default simple
 
+# Abort on errors
+set -e
+
+# Build
+yarn run build
+
+# Add docs
+git add docs/
+git commit -m 'deploy'
+
 # Add coding remote
 git remote add coding git@git.coding.net:yangyang0507/mybatis-plus-doc.git
 git push coding master -f
 
 # Add gitee remote
-git remote add gitee git@gitee.com:baomidou/mybatis-plus-doc.git
-git push gitee master -f
+git remote add github git@github.com:baomidou/mybatis-plus-doc.git
+git push github master -f
