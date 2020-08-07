0c7ade79055e1a97c43b7e2bf94f7848143633f3
D.Yang <koyangslash@gmail.com>
2020-08-07 15:11:25 +0800

    fix build error

--- .vuepress/enhanceApp.js
+++ .vuepress/enhanceApp.js
@@ -1,7 +1,15 @@
-import Vue from "vue";
-import Ads from "vue-google-adsense";
-
-Vue.use(require("vue-script2"));
-Vue.use(Ads.Adsense);
-Vue.use(Ads.InArticleAdsense);
-Vue.use(Ads.InFeedAdsense);
+export default ({ Vue, options, router, siteData }) => {
+  if (typeof window !== "undefined") {
+    import("vue-google-adsense")
+      .then((module) => {
+        const Ads = module.default;
+        Vue.use(require("vue-script2"));
+        Vue.use(Ads.Adsense);
+        Vue.use(Ads.InArticleAdsense);
+        Vue.use(Ads.InFeedAdsense);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+};
