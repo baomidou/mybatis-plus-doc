a354da7cd828664f177e3a44b8661f5402aed8a0
D.Yang <koyangslash@gmail.com>
2022-01-04 18:02:57 +0800

    update ci script

--- deploy.sh
+++ deploy.sh
@@ -3,29 +3,24 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-msg="来自Github Actions的自动部署，更新于$(TZ=UTC-8 date "+%Y-%m-%d %H:%M:%S")"
-codingUrl=https://${CODING_USER}:${CODING_TOKEN}@e.coding.net/yangyang0507/mybatis-plus-doc.git
-dir=docs/.vuepress/dist
-
-git config --global user.name "D.Yang"
-git config --global user.email "koyangslash@gmail.com"
-
-git clone $codingUrl $dir
-
-cd $dir
-rm -rf assets/ blog/ img/ pages/
-cd ../../..
-
 # 生成文档
 yarn run build
 
 # 进入生成的文件夹
-cd $dir
+cd docs/.vuepress/dist
 
 # 部署到Coding
-echo "baomidou.com" > CNAME
+echo "baomidou.com\nwww.baomidou.com" > CNAME
 echo "google.com, pub-4147143076931995, DIRECT, f08c47fec0942fa0" > ads.txt
 
+msg="来自Github Actions的自动部署，更新于$(TZ=UTC-8 date "+%Y-%m-%d %H:%M:%S")"
+codingUrl=https://${CODING_USER}:${CODING_TOKEN}@e.coding.net/yangyang0507/mybatis-plus-doc.git
+
+git config --global user.name "D.Yang"
+git config --global user.email "koyangslash@gmail.com"
+
+git init
+git remote add origin $codingUrl
 git add -A
 git commit -m "${msg}"
 git push -f origin master
