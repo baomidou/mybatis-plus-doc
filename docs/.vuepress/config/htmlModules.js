4d7ebb8f6f734a5b6a37f92318f55809374bc3bb
D.Yang <koyangslash@gmail.com>
2022-11-07 13:58:48 +0800

    update config

--- docs/.vuepress/config/htmlModules.js
+++ docs/.vuepress/config/htmlModules.js
@@ -96,7 +96,8 @@ module.exports = {
         style="display:block"
         data-ad-client="ca-pub-4147143076931995"
         data-ad-slot="1866407052"
-        data-ad-format="auto"></ins>
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
     <script>
         (adsbygoogle = window.adsbygoogle || []).push({});
     </script>
