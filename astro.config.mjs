a5b33df4e5e5342e97c22c9ee02566e0f5061ebb
D.Yang <koyangslash@gmail.com>
2024-05-17 00:08:36 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://baomidou.com",
   integrations: [
     starlight({
       title: "MyBatis-Plus",
