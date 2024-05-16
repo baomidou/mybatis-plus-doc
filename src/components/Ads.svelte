b2e32b7653b309b4347257dabd52246f1a66816b
D.Yang <koyangslash@gmail.com>
2024-05-16 23:51:08 +0800

    update

--- src/components/Ads.svelte
+++ src/components/Ads.svelte
@@ -1,11 +1,11 @@
 <script>
+// WWAD
 </script>
 
-
 <div class="flex xl:flex-col not-content">
-    <div class="w-full xl:flex-1">
-        <div class="max-w-full xl:max-w-[12rem] w-full pt-4 px-4 text-sm leading-6">
-            <div class="wwads-cn wwads-horizontal wwads-sticky border rounded-lg bg-transparent" data-id="135" style="min-height: 0; margin-top: 0;"></div>
-        </div>
+  <div class="w-full xl:flex-1">
+    <div class="max-w-full xl:max-w-[12rem] w-full pt-4 px-4 text-sm leading-6">
+      <div class="wwads-cn wwads-horizontal xl:wwads-vertical wwads-sticky" data-id="135" style="min-height: 0; margin-top: 0; border-width: 1px; border-radius: 0.5rem"></div>
     </div>
+  </div>
 </div>
