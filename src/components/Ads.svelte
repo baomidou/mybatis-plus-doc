34eb54428c397556712ec7e92806c32fc2c24c6c
D.Yang <koyangslash@gmail.com>
2024-05-16 23:26:53 +0800

    update

---
+++ src/components/Ads.svelte
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+<div class="flex xl:flex-col not-content">
+    <div class="w-full xl:flex-1">
+        <div class="max-w-full xl:max-w-[22rem] w-full pt-4 px-4 text-sm leading-6">
+            <div class="wwads-cn wwads-horizontal page-wwads border rounded-lg bg-transparent" data-id="135" style="min-height: 0; margin-top: 0;"></div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .page-wwads .wwads-img img {
+        width: 90px !important;
+    }
+
+    .page-wwads .wwads-content .wwads-text {
+        font-size: 12px !important;
+        line-height: 1 !important;
+    }
+
+    .page-wwads .wwads-poweredby {
+        margin-top: 0 !important;
+    }
+</style>
