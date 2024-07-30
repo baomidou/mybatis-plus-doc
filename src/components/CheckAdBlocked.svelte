a9ae0e177115c5bab47ad397002cd1cf25f66830
D.Yang <koyangslash@gmail.com>
2024-07-30 11:01:39 +0800

    update

--- src/components/CheckAdBlocked.svelte
+++ src/components/CheckAdBlocked.svelte
@@ -13,9 +13,10 @@
     }
 
     // check ad block init
-    if (window._AdBlockInit === undefined) {
-      adBlockDetected = true;
-    }
+    // if (window._AdBlockInit === undefined) {
+    //   adBlockDetected = true;
+    //   return;
+    // }
 
     // check script banned
     checkAdScriptBanned('https://cdn.wwads.cn/js/makemoney.js');
